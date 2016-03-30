e3d9cacc3d5364014127bf0b0d132c9691a86eb2
Josh Cox <josh@webhosting.coop>
2016-03-29 20:15:21 -0500

    more string

--- arduino/hydrobot/hydrobot.ino
+++ arduino/hydrobot/hydrobot.ino
@@ -237,186 +237,6 @@ void myDelay () {
   delay(250);
 }
 
-/*void testlines(uint16_t color) {*/
-  /*tft.fillScreen(ST7735_BLACK);*/
-  /*for (int16_t x=0; x < tft.width(); x+=6) {*/
-    /*tft.drawLine(0, 0, x, tft.height()-1, color);*/
-  /*}*/
-  /*for (int16_t y=0; y < tft.height(); y+=6) {*/
-    /*tft.drawLine(0, 0, tft.width()-1, y, color);*/
-  /*}*/
-
-  /*tft.fillScreen(ST7735_BLACK);*/
-  /*for (int16_t x=0; x < tft.width(); x+=6) {*/
-    /*tft.drawLine(tft.width()-1, 0, x, tft.height()-1, color);*/
-  /*}*/
-  /*for (int16_t y=0; y < tft.height(); y+=6) {*/
-    /*tft.drawLine(tft.width()-1, 0, 0, y, color);*/
-  /*}*/
-
-  /*tft.fillScreen(ST7735_BLACK);*/
-  /*for (int16_t x=0; x < tft.width(); x+=6) {*/
-    /*tft.drawLine(0, tft.height()-1, x, 0, color);*/
-  /*}*/
-  /*for (int16_t y=0; y < tft.height(); y+=6) {*/
-    /*tft.drawLine(0, tft.height()-1, tft.width()-1, y, color);*/
-  /*}*/
-
-  /*tft.fillScreen(ST7735_BLACK);*/
-  /*for (int16_t x=0; x < tft.width(); x+=6) {*/
-    /*tft.drawLine(tft.width()-1, tft.height()-1, x, 0, color);*/
-  /*}*/
-  /*for (int16_t y=0; y < tft.height(); y+=6) {*/
-    /*tft.drawLine(tft.width()-1, tft.height()-1, 0, y, color);*/
-  /*}*/
-/*}*/
-
-/*void testdrawtext(char *text, uint16_t color) {*/
-  /*tft.setCursor(0, 0);*/
-  /*tft.setTextColor(color);*/
-  /*tft.setTextWrap(true);*/
-  /*tft.print(text);*/
-/*}*/
-
-/*void testfastlines(uint16_t color1, uint16_t color2) {*/
-  /*tft.fillScreen(ST7735_BLACK);*/
-  /*for (int16_t y=0; y < tft.height(); y+=5) {*/
-    /*tft.drawFastHLine(0, y, tft.width(), color1);*/
-  /*}*/
-  /*for (int16_t x=0; x < tft.width(); x+=5) {*/
-    /*tft.drawFastVLine(x, 0, tft.height(), color2);*/
-  /*}*/
-/*}*/
-
-/*void testdrawrects(uint16_t color) {*/
-  /*tft.fillScreen(ST7735_BLACK);*/
-  /*for (int16_t x=0; x < tft.width(); x+=6) {*/
-    /*tft.drawRect(tft.width()/2 -x/2, tft.height()/2 -x/2 , x, x, color);*/
-  /*}*/
-/*}*/
-
-/*void testfillrects(uint16_t color1, uint16_t color2) {*/
-  /*tft.fillScreen(ST7735_BLACK);*/
-  /*for (int16_t x=tft.width()-1; x > 6; x-=6) {*/
-    /*tft.fillRect(tft.width()/2 -x/2, tft.height()/2 -x/2 , x, x, color1);*/
-    /*tft.drawRect(tft.width()/2 -x/2, tft.height()/2 -x/2 , x, x, color2);*/
-  /*}*/
-/*}*/
-
-/*void testfillcircles(uint8_t radius, uint16_t color) {*/
-  /*for (int16_t x=radius; x < tft.width(); x+=radius*2) {*/
-    /*for (int16_t y=radius; y < tft.height(); y+=radius*2) {*/
-      /*tft.fillCircle(x, y, radius, color);*/
-    /*}*/
-  /*}*/
-/*}*/
-
-/*void testdrawcircles(uint8_t radius, uint16_t color) {*/
-  /*for (int16_t x=0; x < tft.width()+radius; x+=radius*2) {*/
-    /*for (int16_t y=0; y < tft.height()+radius; y+=radius*2) {*/
-      /*tft.drawCircle(x, y, radius, color);*/
-    /*}*/
-  /*}*/
-/*}*/
-
-/*void testtriangles() {*/
-  /*tft.fillScreen(ST7735_BLACK);*/
-  /*int color = 0xF800;*/
-  /*int t;*/
-  /*int w = tft.width()/2;*/
-  /*int x = tft.height()-1;*/
-  /*int y = 0;*/
-  /*int z = tft.width();*/
-  /*for(t = 0 ; t <= 15; t+=1) {*/
-    /*tft.drawTriangle(w, y, y, x, z, x, color);*/
-    /*x-=4;*/
-    /*y+=4;*/
-    /*z-=4;*/
-    /*color+=100;*/
-  /*}*/
-/*}*/
-
-/*void testroundrects() {*/
-  /*tft.fillScreen(ST7735_BLACK);*/
-  /*int color = 100;*/
-  /*int i;*/
-  /*int t;*/
-  /*for(t = 0 ; t <= 4; t+=1) {*/
-    /*int x = 0;*/
-    /*int y = 0;*/
-    /*int w = tft.width()-2;*/
-    /*int h = tft.height()-2;*/
-    /*for(i = 0 ; i <= 16; i+=1) {*/
-      /*tft.drawRoundRect(x, y, w, h, 5, color);*/
-      /*x+=2;*/
-      /*y+=3;*/
-      /*w-=4;*/
-      /*h-=6;*/
-      /*color+=1100;*/
-    /*}*/
-    /*color+=100;*/
-  /*}*/
-/*}*/
-
-/*void tftPrintTest() {*/
-  /*tft.setTextWrap(false);*/
-  /*tft.fillScreen(ST7735_BLACK);*/
-  /*tft.setCursor(0, 30);*/
-  /*tft.setTextColor(ST7735_RED);*/
-  /*tft.setTextSize(1);*/
-  /*tft.println("Hello World!");*/
-  /*tft.setTextColor(ST7735_YELLOW);*/
-  /*tft.setTextSize(2);*/
-  /*tft.println("Hello World!");*/
-  /*tft.setTextColor(ST7735_GREEN);*/
-  /*tft.setTextSize(3);*/
-  /*tft.println("Hello World!");*/
-  /*tft.setTextColor(ST7735_BLUE);*/
-  /*tft.setTextSize(4);*/
-  /*tft.print(1234.567);*/
-  /*delay(1500);*/
-  /*tft.setCursor(0, 0);*/
-  /*tft.fillScreen(ST7735_BLACK);*/
-  /*tft.setTextColor(ST7735_WHITE);*/
-  /*tft.setTextSize(0);*/
-  /*tft.println("Hello World!");*/
-  /*tft.setTextSize(1);*/
-  /*tft.setTextColor(ST7735_GREEN);*/
-  /*tft.println(" Want pi?");*/
-  /*tft.println(" ");*/
-  /*tft.print(8675309, HEX); // print 8,675,309 out in HEX!*/
-  /*tft.println(" Print HEX!");*/
-  /*tft.println(" ");*/
-  /*tft.setTextColor(ST7735_WHITE);*/
-  /*tft.println("Sketch has been");*/
-  /*tft.println("running for: ");*/
-  /*tft.setTextColor(ST7735_MAGENTA);*/
-  /*tft.print(millis() / 1000);*/
-  /*tft.setTextColor(ST7735_WHITE);*/
-  /*tft.print(" seconds.");*/
-/*}*/
-
-/*void mediabuttons() {*/
-  /*// play*/
-  /*tft.fillScreen(ST7735_BLACK);*/
-  /*tft.fillRoundRect(25, 10, 78, 60, 8, ST7735_WHITE);*/
-  /*tft.fillTriangle(42, 20, 42, 60, 90, 40, ST7735_RED);*/
-  /*delay(500);*/
-  /*// pause*/
-  /*tft.fillRoundRect(25, 90, 78, 60, 8, ST7735_WHITE);*/
-  /*tft.fillRoundRect(39, 98, 20, 45, 5, ST7735_GREEN);*/
-  /*tft.fillRoundRect(69, 98, 20, 45, 5, ST7735_GREEN);*/
-  /*delay(500);*/
-  /*// play color*/
-  /*tft.fillTriangle(42, 20, 42, 60, 90, 40, ST7735_BLUE);*/
-  /*delay(50);*/
-  /*// pause color*/
-  /*tft.fillRoundRect(39, 98, 20, 45, 5, ST7735_RED);*/
-  /*tft.fillRoundRect(69, 98, 20, 45, 5, ST7735_RED);*/
-  /*// play color*/
-  /*tft.fillTriangle(42, 20, 42, 60, 90, 40, ST7735_GREEN);*/
-/*}*/
-
 const float pi = 3.1415926;
 
 bool checkThrottle(unsigned long throttle, int dog){
@@ -461,7 +281,7 @@ bool serialcheckThrottle(unsigned long throttle, int dog){
     // return one or 'ok'
     return 1;
   }
-  else if( dog > 3000 ){
+  else if( dog > 40 ){
     // return one or 'ok'
     return 1;
   }
@@ -563,11 +383,25 @@ void printOutput () {
   dataString += "DHT1122 ";
   dataString += " aveOff= ";
   dataString += "Mean:   "; 
-  dataString += String(int((aveOff.mean())));
+  dataString += String(int(aveOff.mean()));
   dataString += "Mode:   "; 
-  dataString += String(int((aveOff.mode())));
+  dataString += String(int(aveOff.mode()));
   dataString += "StdDev: "; 
-  dataString += String(int((aveOff.stddev())));
+  dataString += String(int(aveOff.stddev()));
+  dataString += "dryL= ";
+  dataString += String(dryLimit);
+  dataString += "wetL= ";
+  dataString += String(wetLimit);
+  dataString += "\r\n";
+  dataString += "countOff= ";
+  dataString += String(countdownOff());
+  dataString += "onCountMin= ";
+  dataString += String(onCountMin());
+  dataString += "pumpOnCount= ";
+  dataString += String(pumpOnCount);
+  dataString += "PIDoutput= ";
+  dataString += String(Output);
+  dataString += "\r\n";
   dataString += "3478-ENDTRANSMISSION";
   dataString += "\r\n";
   Serial.print(dataString);
