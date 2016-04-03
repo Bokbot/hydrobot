0a712a1917dcaf1fb65f6df8a3ffd632983a4622
Josh Cox <josh@webhosting.coop>
2016-04-03 17:53:47 -0500

    ph

--- arduino/hydrobot/hydrobot.ino
+++ arduino/hydrobot/hydrobot.ino
@@ -32,8 +32,6 @@ String dataString = "";
 #include <Wire.h>
 #define ezophaddress 99               //default I2C ID number for EZO pH Circuit.
 
-
-
 char computerdata[20];           //we make a 20 byte character array to hold incoming data from a pc/mac/other.   
 byte received_from_computer=0;   //we need to know how many characters have been received.    
 byte serial_event=0;             //a flag to signal when data has been received from the pc/mac/other. 
@@ -100,11 +98,6 @@ void slavesRespond(){
 #define moisture_input 0
 #define divider_top 12
 #define divider_bottom 13
-
-const int VAL_PROBE1 = 0; // Analog pin 0
-const int VAL_PROBE2 = 1; // Analog pin 1
-const int VAL_PROBE3 = 2; // Analog pin 2
-const int VAL_PROBE4 = 3; // Analog pin 3
 
 //Define Variables we'll be connecting to
 double Setpoint, Input, Output;
@@ -367,8 +360,80 @@ void directOutput ( int inputValue ) {
   //analogWrite(analogOutPin, outputValue);
 }
 
+//------------PrintFloat---------------
+//void printFloat(float value, int places) {
+String printFloat(float value, int places) {
+  // this is used to cast digits
+  String returnString = "";
+  int digit;
+  float tens = 0.1;
+  int tenscount = 0;
+  int i;
+  float tempfloat = value;
+
+  // if value is negative, set tempfloat to the abs value
+  // make sure we round properly. this could use pow from  
+  //<math.h>, but doesn't seem worth the import
+  // if this rounding step isn't here, the value  54.321 prints as  
+  //54.3209
+
+  // calculate rounding term d:   0.5/pow(10,places)
+  float d = 0.5;
+  if (value < 0)
+       d *= -1.0;
+  // divide by ten for each decimal place
+  for (i = 0; i < places; i++)
+       d/= 10.0;
+  // this small addition, combined with truncation will round our  
+  // values properly
+  tempfloat +=  d;
+
+  // first get value tens to be the large power of ten less than value
+  // tenscount isn't necessary but it would be useful if you wanted  
+  // to know after this how many chars the number will take
+
+  if (value < 0)
+       tempfloat *= -1.0;
+  while ((tens * 10.0) <= tempfloat) {
+       tens *= 10.0;
+       tenscount += 1;
+  }
+
+  // write out the negative if needed
+  if (value < 0)
+       returnString += '-';
+
+  if (tenscount == 0)
+       returnString += '0';
+
+  for (i=0; i< tenscount; i++) {
+       digit = (int) (tempfloat/tens);
+       returnString += digit;
+       tempfloat = tempfloat - ((float)digit * tens);
+       tens /= 10.0;
+  }
+
+  // if no places after decimal, stop now and return
+  if (places <= 0)
+       return returnString;
+
+  // otherwise, write the point and continue on
+  returnString += '.';
+
+  // now write out each decimal place by shifting digits one by one  
+  // into the ones place and writing the truncated value
+  for (i = 0; i < places; i++) {
+    tempfloat *= 10.0;
+    digit = (int) tempfloat;
+    returnString += digit;
+    // once written, subtract off that digit
+    tempfloat = tempfloat - (float) digit;
+  }
+  return returnString;
+}
+
 void printOutput () {
-  // ardushipper 
+  // ardushipper
      // make a string for assembling the data to log:
   /*dataString += String(int((10 * moisture1)));*/
   /*dataString += String(int((10 * moisture2)));*/
@@ -389,6 +454,8 @@ void printOutput () {
   dataString += "\r\n";
   // Moisture
   //Serial.print("DHT1122-Moisture1 ");
+  dataString += "monit-pH ";
+  dataString += printFloat(ph_float, 3);
   dataString += "DHT1122-Moisture1 ";
   //Serial.println(sensorValue1);
   dataString += String(int((moistureValue1)));
@@ -581,13 +648,74 @@ int SoilMoisture(){
 
 // LCD init END
 
+void serialEvent(){            //this interrupt will trigger when the data coming from the serial monitor(pc/mac/other) is received.    
+  received_from_computer=Serial.readBytesUntil(13,computerdata,20); //we read the data sent from the serial monitor(pc/mac/other) until we see a <CR>. We also count how many characters have been received.      
+  computerdata[received_from_computer]=0;  //stop the buffer from transmitting leftovers or garbage.
+  serial_event=1;
+}
+
+char *ftoa(char *a, double f, int precision){
+  long p[] = {0,10,100,1000,10000,100000,1000000,10000000,100000000};
+  char *ret = a;
+  long heiltal = (long)f;
+  itoa(heiltal, a, 10);
+  while (*a != '\0') a++;
+  *a++ = '.';
+  long desimal = abs((long)((f - heiltal) * p[precision]));
+  itoa(desimal, a, 10);
+  return ret;
+}
+
+float readpH() {
+    float internal_ph_float;                  //float var used to hold the float value of the pH. 
+    time_=1800;
+    Wire.beginTransmission(ezophaddress); //call the circuit by its ID number.
+    Wire.write('r');        //transmit the command that was sent through the serial port.
+    Wire.endTransmission();          //end the I2C data transmission.
+    delay(time_);                    //wait the correct amount of time for the circuit to complete its instruction.
+    Wire.requestFrom(ezophaddress,20,1); //call the circuit and request 20 bytes (this may be more than we need)
+    code=Wire.read();               //the first byte is the response code, we read this separately.
+    switch (code){                  //switch case based on what the response code is.
+      case 1:                       //decimal 1.
+    //    Serial.println("Success");  //means the command was successful.
+      break;                        //exits the switch case.
+
+     case 2:                        //decimal 2.
+     //  Serial.println("Failed");    //means the command has failed.
+     break;                         //exits the switch case.
+
+     case 254:                      //decimal 254.
+      // Serial.println("Pending");   //means the command has not yet been finished calculating.
+     break;                         //exits the switch case.
+
+     case 255:                      //decimal 255.
+      // Serial.println("No Data");   //means there is no further data to send.
+     break;                         //exits the switch case.
+    }
+    while(Wire.available()){          //are there bytes to receive.
+     in_char = Wire.read();           //receive a byte.
+     ph_data[i]= in_char;             //load this byte into our array.
+     i+=1;                            //incur the counter for the array element.
+      if(in_char==0){                 //if we see that we have been sent a null command.
+          i=0;                        //reset the counter i to 0.
+          Wire.endTransmission();     //end the I2C data transmission.
+          break;                      //exit the while loop.
+      }
+    }
+    //Serial.println(ph_data);          //print the data.
+    internal_ph_float=atof(ph_data);
+    return internal_ph_float;
+}
+
 void setup() {
   // Start the I2C Bus as Slave  
-  Wire.begin(SlaveDeviceId);      // join i2c bus with Slave ID
+  //Wire.begin(SlaveDeviceId);      // join i2c bus with Slave ID
+  // Start the I2C Bus as Master
+  Wire.begin();      // join i2c bus as master
   // Attach a function to trigger when something is received.
-  Wire.onReceive(receiveDataPacket); // register talk event
+  //Wire.onReceive(receiveDataPacket); // register talk event
   // Attach a function to trigger when something is requested.
-  Wire.onRequest(slavesRespond);  // register callback event
+  //Wire.onRequest(slavesRespond);  // register callback event
   // initialize serial communications at 9600 bps:
   throttleTime = (millis() + 30000); // 30,000 ms = 30 seconds
   serialthrottleTime = (millis() + 5);
@@ -677,6 +805,7 @@ void setup() {
   //turn the PID on
   myPID.SetMode(AUTOMATIC);
   // PID end
+  ph_float = readpH();
 
   /*turnOffPump();*/
   /*delay(1000);*/
@@ -701,6 +830,45 @@ void loop() {
   Input = analogRead(PIN_INPUT);
   myPID.Compute();
 
+  /*if(serial_event){            //if the serial_event=1.*/
+    /*if(computerdata[0]=='c'||computerdata[0]=='r')time_=1800; //if a command has been sent to calibrate or take a reading we wait 1800ms so that the circuit has time to take the reading.  */
+    /*else time_=300;         //if any other command has been sent we wait only 300ms.*/
+    /*Wire.beginTransmission(address); //call the circuit by its ID number.*/
+    /*Wire.write(computerdata);        //transmit the command that was sent through the serial port.*/
+    /*Wire.endTransmission();          //end the I2C data transmission.*/
+    /*delay(time_);                    //wait the correct amount of time for the circuit to complete its instruction.*/
+    /*Wire.requestFrom(address,20,1); //call the circuit and request 20 bytes (this may be more than we need)*/
+    /*code=Wire.read();               //the first byte is the response code, we read this separately.*/
+    /*switch (code){                  //switch case based on what the response code is.*/
+      /*case 1:                       //decimal 1.*/
+        /*Serial.println("Success");  //means the command was successful.*/
+      /*break;                        //exits the switch case.*/
+
+     /*case 2:                        //decimal 2.*/
+       /*Serial.println("Failed");    //means the command has failed.*/
+     /*break;                         //exits the switch case.*/
+
+     /*case 254:                      //decimal 254.*/
+       /*Serial.println("Pending");   //means the command has not yet been finished calculating.*/
+     /*break;                         //exits the switch case.*/
+
+     /*case 255:                      //decimal 255.*/
+       /*Serial.println("No Data");   //means there is no further data to send.*/
+     /*break;                         //exits the switch case.*/
+    /*}*/
+    /*while(Wire.available()){          //are there bytes to receive.*/
+     /*in_char = Wire.read();           //receive a byte.*/
+     /*ph_data[i]= in_char;             //load this byte into our array.*/
+     /*i+=1;                            //incur the counter for the array element.*/
+      /*if(in_char==0){                 //if we see that we have been sent a null command.*/
+          /*i=0;                        //reset the counter i to 0.*/
+          /*Wire.endTransmission();     //end the I2C data transmission.*/
+          /*break;                      //exit the while loop.*/
+      /*}*/
+    /*}*/
+    /*Serial.println(ph_data);          //print the data.*/
+    /*serial_event=0;                   //reset the serial event flag.*/
+  /*}*/
   /************************************************
    * turn the output pin on/off based on pid output
    ************************************************/
@@ -741,6 +909,7 @@ void loop() {
   // Serial.print(ok);
   // Serial.println(" = ok");
   if(ok2 == 1) {
+    ph_float = readpH();
     printOutput();
     ok2 = 0;
     serialthrottleTime = (millis() + 5000); // 5,000 ms = 5 seconds
