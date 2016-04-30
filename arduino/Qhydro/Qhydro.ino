488fac7cfacdbae05b82b4f4e55f9abe5b779b77
Josh Cox <josh@webhosting.coop>
2016-04-30 17:48:01 -0500

    addedDallas

--- arduino/Qhydro/Qhydro.ino
+++ arduino/Qhydro/Qhydro.ino
@@ -40,18 +40,77 @@
 
 #include <SPI.h>
 #include <MySensor.h>
+#include <DallasTemperature.h>
+#include <OneWire.h>
+
+#define COMPARE_TEMP 1 // Send temperature only if changed? 1 = Yes 0 = No
+
+#define ONE_WIRE_BUS 3 // Pin where dallase sensor is connected 
+#define MAX_ATTACHED_DS18B20 16
+unsigned long SLEEP_TIME = 30000; // Sleep time between reads (in milliseconds)
+OneWire oneWire(ONE_WIRE_BUS); // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+DallasTemperature sensors(&oneWire); // Pass the oneWire reference to Dallas Temperature. 
+float lastTemperature[MAX_ATTACHED_DS18B20];
+int numSensors=0;
+boolean receivedConfig = false;
+boolean metric = true; 
+// Initialize temperature message
+MyMessage msg(0,V_TEMP);
 
 void setup() {
-  
+  // DallasTemperature setup
+  // Startup up the OneWire library
+  sensors.begin();
+  // requestTemperatures() will not block current thread
+  sensors.setWaitForConversion(false);
 }
 
 void presentation()  
 {  
   //Send the sensor node sketch version information to the gateway
-  sendSketchInfo("Repeater Node", "1.0");
+  //sendSketchInfo("Repeater Node", "1.0");
+  // Send the sketch version information to the gateway and Controller
+  sendSketchInfo("Monitaur Multi HydroQ Sensor", "1.1");
+
+  // DallasTemperature presentation
+  // Fetch the number of attached temperature sensors  
+  numSensors = sensors.getDeviceCount();
+
+  // Present all sensors to controller
+  for (int i=0; i<numSensors && i<MAX_ATTACHED_DS18B20; i++) {   
+     present(i, S_TEMP);
+  }
 }
 
 void loop() 
 {
+  // Fetch temperatures from Dallas sensors
+  sensors.requestTemperatures();
+
+  // query conversion time and sleep until conversion completed
+  int16_t conversionTime = sensors.millisToWaitForConversion(sensors.getResolution());
+  // sleep() call can be replaced by wait() call if node need to process incoming messages (or if node is repeater)
+  sleep(conversionTime);
+
+  // Read temperatures and send them to controller 
+  for (int i=0; i<numSensors && i<MAX_ATTACHED_DS18B20; i++) {
+ 
+    // Fetch and round temperature to one decimal
+    float temperature = static_cast<float>(static_cast<int>((getConfig().isMetric?sensors.getTempCByIndex(i):sensors.getTempFByIndex(i)) * 10.)) / 10.;
+ 
+    // Only send data if temperature has changed and no error
+    #if COMPARE_TEMP == 1
+    if (lastTemperature[i] != temperature && temperature != -127.00 && temperature != 85.00) {
+    #else
+    if (temperature != -127.00 && temperature != 85.00) {
+    #endif
+ 
+      // Send in the new temperature
+      send(msg.setSensor(i).set(temperature,1));
+      // Save new temperatures for next compare
+      lastTemperature[i]=temperature;
+    }
+  }
+  sleep(SLEEP_TIME);
 }
 
